258d1f63e482b8f5c50c6cb4e94a1e2cb0c0d852
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-18 15:09:50 -0300

    feat: add input create task

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,29 @@
-import { View, Text } from 'react-native'
-import { styles } from './styles'
-import React from 'react'
+import {
+  Text,
+  SafeAreaView,
+  Image,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { styles } from './styles';
+import React from 'react';
+import { theme } from '../../theme';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.Container}>
+      <Image source={require('../../../assets/logo.png')} />
+      {/* add task input */}
+      <View style={styles.InputContainer}>
+        <TextInput placeholder='Título da tarefa...' placeholderTextColor={theme.colors.base.gray600} style={styles.Input}/>
+        <TouchableOpacity style={styles.addBtn}>
+          <Text style={styles.addBtnText}>+</Text>
+        </TouchableOpacity>
+      </View>
 
-export default HomeScreen
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,36 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme';
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor: theme.colors.base.gray900,
+    paddingHorizontal: 35,
+    paddingVertical: 45,
+    gap: 40,
+    alignItems: 'center',
+  },
+  InputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 6,
+  },
+  Input: {
+    height: 40,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: theme.colors.base.gray600,
+    paddingHorizontal: 12,
+    flex: 1,
+  },
+  addBtn: {
+    borderRadius: 5,
+    height: 40,
+    backgroundColor: theme.colors.base.indigo,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addBtnText: { color: theme.colors.base.gray50 },
+});
