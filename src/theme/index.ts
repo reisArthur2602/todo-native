f190285d499e99544002f426a9018539b3c85d2d
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-18 12:40:57 -0300

    feat: add theme config e Dto task

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export const theme = {
+	colors: {
+		base: {
+			gray900: '#09090B',
+			gray800: '#18181B',
+			gray700: '#171717',
+			gray600: '#3F3F46',
+			gray50: '#FAFAFA',
+			gray100: '#A1A1AA',
+			indigo: '#4338CA',
+	
+		},
+	},
+	font_family: {
+		regular: 'Inter_400Regular',
+		semibold: 'Inter_600SemiBold',
+		medium: 'Inter_600Medium',
+	},
+	font_size: {
+		sm: 12,
+		md: 14,
+		lg: 16,
+	},
+}
