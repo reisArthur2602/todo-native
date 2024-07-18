084a71341f3249e0fc5a5e0b4ac89f7bebad9419
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-18 12:44:53 -0300

    feat: add fonte inter

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Routes from './src/routes';
 
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/inter';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+  });
   return (
     <>
-      <StatusBar style="light" translucent/>
-      <Routes/>
+      {fontsLoaded && <Routes />}
+      <StatusBar style="light" translucent />
     </>
   );
 }
